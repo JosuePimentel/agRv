0ab793ca4dd7128c1b800bfcb22dd398091c70f9
JosuePimentel <josue.farias.pimentel@gmail.com>
2025-01-22 20:48:44 -0300

    footer

--- agRv.Frontend/src/app/components/footer/footer.component.ts
+++ agRv.Frontend/src/app/components/footer/footer.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { SocialMediaLinkComponent } from '../social-media-link/social-media-link.component';
+import { SmallLogoComponent } from '../small-logo/small-logo.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [LogoComponent, SocialMediaLinkComponent],
+  imports: [LogoComponent, SocialMediaLinkComponent, SmallLogoComponent],
   templateUrl: './footer.component.html',
   host: {
-    class: 'flex justify-between py-[40px] mt-[40px] items-center'
+    class: 'w-full flex flex-col bg-black rounded-t-[20px] relative after:content-[""] after:absolute after:-top-2 after:-z-10  after:left-0 after:bg-PRIMARY-default after:w-full after:h-6'
   }
 })
-export class FooterComponent {
-  socialMediaLinks: any[] = [
-    {
-      link: "",
-      type: "instagram"
-    },
-    {
-      link: "",
-      type: "X"
-    }
-  ]
-}
+export class FooterComponent {}
